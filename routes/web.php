a10e400b413cab4b932e72fbc460a884a6770708
Eduardo H. García <91917354+waryplumblum@users.noreply.github.com>
2024-06-14 14:49:30 -0600

    feat: Unique URLs for walls

--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,6 @@ Route::post('/register', [RegisterController::class,'store']);
 
 Route::get('/login',[LoginController::class,'index'])->name('login');
 Route::post('/login',[LoginController::class,'store']);
-Route::post('logout',[LogoutController::class,'store'])->name('logout');
+Route::post('/logout',[LogoutController::class,'store'])->name('logout');
 
-Route::get('muro',[PostController::class,'index'])->name('posts.index');
+Route::get('/{user:username}',[PostController::class,'index'])->name('posts.index');
