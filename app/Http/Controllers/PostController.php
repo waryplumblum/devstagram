df4641b1c117713200eb92c3558bf23bf29e5128
Eduardo H. García <91917354+waryplumblum@users.noreply.github.com>
2024-06-17 10:26:48 -0600

    feat: first steps to create new posts

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -15,6 +15,6 @@ class PostController extends Controller
        return view('dashboard', ['user' => $user]);
     }
     public function create(){
-        
+        return view('posts.create');
     }
 }
