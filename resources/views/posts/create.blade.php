86c8b34b7c58be86c123fb2f38705bf48a6e9771
Eduardo H. García <91917354+waryplumblum@users.noreply.github.com>
2024-06-17 10:49:56 -0600

    feat: finalizing the publication form

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -13,26 +13,50 @@
             <form action="{{route('register')}}" method="POST" novalidate>
                 @csrf
                 <div class="mb-5">
-                    <label  for="name"
+                    <label  for="titulo"
                             class="mb-2 block uppercase text-gray-500 font-bold">
-                        Nombre
+                        Titulo
                     </label>
-                    <input  id="name"
-                            name="name"
+                    <input  id="titulo"
+                            name="titulo"
                             type="text"
-                            placeholder="Tu Nombre"
+                            placeholder="Titulo de la Publicación"
                             class="border p-3 w-full rounded-lg @error('name')
                                 border-red-500
                             @enderror"
-                            value="{{old('name')}}"
+                            value="{{old('titulo')}}"
                     />
 
-                    @error('name')
+                    @error('titulo')
                         <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">
                             {{ $message }}
                         </p>
                     @enderror
                 </div>
+                <div class="mb-5">
+                    <label  for="descripcion"
+                            class="mb-2 block uppercase text-gray-500 font-bold">
+                        Descripción
+                    </label>
+                    <textarea
+                        id="descripcion"
+                        name="descripcion"
+                        placeholder="Descripción de la Publicación"
+                        class="border p-3 w-full rounded-lg
+                            @error('name')
+                                border-red-500
+                            @enderror"
+                    >{{old('titulo')}}</textarea>
+
+                    @error('titulo')
+                        <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">
+                            {{ $message }}
+                        </p>
+                    @enderror
+                </div>
+                <input  type="submit"
+                        value="Crear Publicación"
+                        class="bg-sky-600 hover:bg-sky-700 transition-colors cursor-pointer uppercase font-bold w-full p-3 text-white rounded-lg">
             </form>
         </div>
     </div>
