09df994b8e1e00acfd62ca7328341b2e0febdccc
Eduardo H. García <91917354+waryplumblum@users.noreply.github.com>
2024-06-19 13:17:53 -0600

    feat: adding intervention to process images p2

--- app/Http/Controllers/ImagenController.php
+++ app/Http/Controllers/ImagenController.php
@@ -2,12 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+
+use Intervention\Image\ImageManager;
+use Intervention\Image\Drivers\Imagick\Driver;
 
 class ImagenController extends Controller
 {
     public function store(Request $request){
         $imagen = $request->file('file');
-        return response()->json(['imagen' => $imagen->extension()]);
+
+        $nombreImagen = Str::uuid() . "." . $imagen->extension();
+
+        $manager = new ImageManager(new Driver());
+        $imagenServidor = $manager::imagick()->read($imagen);
+        $imagenServidor->resizeDown(1000, 1000);
+
+        $imagenPath = public_path('uploads') . '/' . $nombreImagen;
+
+        $imagenServidor->toPng()->save($imagenPath);
+
+        return response()->json([
+            'imagen' => $nombreImagen,
+        ]);
     }
 }
--- config/app.php
+++ config/app.php
@@ -169,7 +169,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
-        Intervention\Image\ImageServiceProvider::class
+        //Intervention\Image\ImageServiceProvider::class
 
     ])->toArray(),
 
@@ -186,7 +186,7 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'Example' => App\Facades\Example::class,
-        'Image' => Intervention\Image\Facades\Image::class
+        //'Image' => Intervention\Image\Facades\Image::class
 
     ])->toArray(),
 
